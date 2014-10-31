e8be8e44bd88108f5c5d97d6f362bfc0ddd4336c
jhonata menezes <jhonatamenezes10@gmail.com>
2014-10-31 16:52:31 -0200

    funcao para converter segundos em minutos adicionada

--- js/searchSoundCloud.js
+++ js/searchSoundCloud.js
@@ -6,7 +6,7 @@ function getSearch()
 function searchAudio(search)
 {
 	url = 'http://api.soundcloud.com/search';
-	limitRows = parseInt($('#limit').val())+1;
+	limitRows = parseInt($('#limit').val());
 	$.getJSON(url, {q: search, client_id: clientId(), format: 'json', limit: limitRows }).done(function(data){
 		$.each(data.collection, function(i, item){
 			$('.list-music').prepend(verifyExistsAudio(item));
@@ -29,8 +29,9 @@ function verifyExistsAudio(item)
 	}
 	else
     {
+    	duracao = new String(item.duration);
     	link = $.trim(item.stream_url+'?client_id='+clientId());
-    	return '<a class="list-group-item list-item-color" onclick="createPlayer(\''+link+'\')" > '+ item.title +'</a>';
+    	return '<a class="list-group-item list-item-color" onclick="createPlayer(\''+link+'\')" > '+ item.title +' <span class="pull-right"><b> Duração: '+ formatTime(parseInt(duracao.substring(0,(duracao.length-3)))) +' Segundos Tamanho: '+ Math.round(parseInt(item.original_content_size)/1024) +'MB <b></span></a>';
     }
 	
 }
@@ -39,3 +40,25 @@ function clearDiv(div)
 {
 	$(div).empty();
 }
+
+function formatTime(secs){
+   var times = new Array(3600, 60, 1);
+   var time = '';
+   var tmp;
+   for(var i = 0; i < times.length; i++){
+      tmp = Math.floor(secs / times[i]);
+      if(tmp < 1){
+         tmp = '00';
+      }
+      else if(tmp < 10){
+         tmp = '0' + tmp;
+      }
+      time += tmp;
+      if(i < 2){
+         time += ':';
+      }
+      secs = secs % times[i];
+   }
+   return time;
+}
+
